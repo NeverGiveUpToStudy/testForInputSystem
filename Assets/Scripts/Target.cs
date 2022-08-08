7d7e6507259ea6423a195d180c16ed2090ee9bbe
NeverGiveUpToStudy <wuliangjun007@qq.com>
2022-08-08 16:27:37 +0800

    移除宏相关的配置,用更优质的代码替代原来的操作

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,28 +2,32 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Target : MonoBehaviour {
-    private Rigidbody targetRb;
     private float minSpeed = 14;
     private float maxSpeed = 18;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+
+    private Rigidbody targetRb;
     private GameManager gameManager;
+    private Camera cam;
+    private InputAction doingPositionAction;
 
     public int pointValue;
     public ParticleSystem explosionParticle;
 
-    private Camera cam;
-
-    // Start is called before the first frame update
     private void Start() {
-        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
 
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        PlayerInput inputActions = GetComponent<PlayerInput>();
+        doingPositionAction = inputActions.actions.FindAction("Position");
     }
 
     private Vector3 RandomForce() {
@@ -39,35 +43,46 @@ public class Target : MonoBehaviour {
     }
 
     public void OnClick(InputAction.CallbackContext context) {
-        RaycastHit hit;
+        //#if UNITY_STANDALONE || UNITY_EDITOR
+        //        Vector3 coor = Mouse.current.position.ReadValue();
+        //#elif UNITY_ANDROID || UNITY_IOS
+        //        Vector3 coor = Touchscreen.current.position.ReadValue();
+        //#endif
 
+        //        if (Physics.Raycast(cam.ScreenPointToRay(coor), out hit)) {
+        //            if (hit.collider.gameObject == gameObject)
+        //                if (gameManager.isGameActive) {
+        //                    Destroy(gameObject);
+        //                    Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        //                    gameManager.UpdateScore(pointValue);
+        //                }
+        //        }
 
-#if UNITY_STANDALONE || UNITY_EDITOR
-        Vector3 coor = Mouse.current.position.ReadValue();
-#elif UNITY_ANDROID || UNITY_IOS
-        Vector3 coor = Touchscreen.current.position.ReadValue();
-#endif
+        //        if (context.phase == InputActionPhase.Started) {
+        //            Debug.Log("点击Started");
+        //        } else if (context.phase == InputActionPhase.Waiting) {
+        //            Debug.Log("点击Waiting");
+        //        } else if (context.phase == InputActionPhase.Performed) {
+        //            Debug.Log("点击Performed");
+        //        } else if (context.phase == InputActionPhase.Canceled) {
+        //            Debug.Log("点击Canceled");
+        //        } else if (context.phase == InputActionPhase.Disabled) {
+        //            Debug.Log("点击Disabled");
+        //        }
 
-        if (Physics.Raycast(cam.ScreenPointToRay(coor), out hit)) {
-            if (hit.collider.gameObject == gameObject)
-                if (gameManager.isGameActive) {
-                    Destroy(gameObject);
-                    Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-                    gameManager.UpdateScore(pointValue);
-                }
-        }
-
-        if (context.phase == InputActionPhase.Started) {
-            Debug.Log("点击Started");
-        } else if (context.phase == InputActionPhase.Waiting) {
-            Debug.Log("点击Waiting");
-        } else if (context.phase == InputActionPhase.Performed) {
-            Debug.Log("点击Performed");
-        } else if (context.phase == InputActionPhase.Canceled) {
-            Debug.Log("点击Canceled");
-        } else if (context.phase == InputActionPhase.Disabled) {
-            Debug.Log("点击Disabled");
-        }
+        if (context.phase == InputActionPhase.Performed) {
+            RaycastHit hit;
+            Vector3 coor = doingPositionAction.ReadValue<Vector2>();
+            if (Physics.Raycast(cam.ScreenPointToRay(coor), out hit)) {
+                if (hit.collider.gameObject == gameObject)
+                    if (gameManager.isGameActive) {
+                        Destroy(gameObject);
+                        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+                        gameManager.UpdateScore(pointValue);
+                    }
+            }
+        } else
+            Debug.Log("别的动作暂不处理");
     }
 
     private void OnTriggerEnter(Collider other) {
